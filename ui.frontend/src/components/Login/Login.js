4ff13c78d56cd4960c5f58f64da12d0d4ee7a37c
Rafael Brandao Bastos <rafael.brandao@compasso.com.br>
2023-03-24 13:08:23 -0300

    Chore: Creating Countainers & Props

--- ui.frontend/src/components/Login/Login.js
+++ ui.frontend/src/components/Login/Login.js
@@ -1,19 +1,78 @@
 import React from 'react';
 import { MapTo } from '@adobe/aem-react-editable-components';
 import { useHistory } from 'react-router-dom'; 
-import {Container} from "./style";
+import {Container, Img, SubContainer, LoginContainer} from "./style";
 
-const Login = () => {
-    const history = useHistory()
+const Login = ({
+
+    backgroundImage,
+    titleColor,
+    titleSize,
+    title,
+    subTitleColor,
+    subTitleSize,
+    subTitle,
+    textColor,
+    textSize,
+    text,
+    label,
+    labelSize,
+    labelColor,
+    buttonColor,
+    inputUserText,
+    inputUserColor,
+    inputPasswordText,
+    inputPasswordColor
+
+}) => {
+
+    {/*const history = useHistory()
     function handleClick() {
         history.push("/content/reactapp/us/en/home0.html");
-    }
+    }*/}
+
     return(
         <Container>
-            
+            <SubContainer>
+            <LoginContainer>
+                <div style={{display: "block", height: "30%"}}>
+                   
+                </div>
+                
+                <div>
+                    
+                </div>
+
+            </LoginContainer>             
+            </SubContainer>
+            <Img src={backgroundImage.src} alt="ImageComponent"/>
             {/*<button onClick = {handleClick}>Prox</button>
             <p>Login</p>*/} 
         </Container>
                     
     )
-}; export default MapTo("reactapp/components/login-component")(Login);
+};Login.defaultProps = {
+    textcontent: [ 
+        {  
+            inputUsercolor: "#E0E0E0",
+            inputUserText: "Usuário",
+            inputPasswordcolor: "#E0E0E0",
+            inputPasswordText: "Senha",
+            titleColor: "#E0E0E0",
+            titleSize: "60px" ,
+            title: "Olá",
+            subTitleColor: "#E0E0E0",
+            subTitleSize: "30px" ,
+            subTitle: "Olá",
+            textColor: "#E0E0E0",
+            textSize: "16px" ,
+            text: "Para continuar navegando de forma segura, efetue o login na rede.",
+            buttonColor: "#FF2D04",
+            labelColor: "#FFFFFF" ,
+            llabelSize: "18px", 
+            label: "Continuar"    
+        },
+    ],
+}; 
+
+export default  MapTo("reactapp/components/login-component")(Login);
--- ui.frontend/src/components/Login/style.js
+++ ui.frontend/src/components/Login/style.js
@@ -3,6 +3,41 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     width: 100vw;
+    height: 100vh;   
+`;
+
+export const SubContainer = styled.div`
+    display: flex;
+    width: 50%;
     height: 100vh;
-    background-color: blue;    
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(#33383D, #1C1D20);
+    @media (max-width: 768px) {
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+    }   
+`;
+export const LoginContainer = styled.div`
+    display: flex;
+    width: 380px;
+    height: 80%;
+    max-height: 650px;
+    justify-content: space-between;
+    align-items: flex-start; 
+    flex-direction: column; 
+    @media (max-width: 400px) {
+        width: 82%;
+        align-items: center;
+    }
+`;
+
+export const Img = styled.img`
+    object-fit: cover;
+    width: 50%;
+    height: auto;
+    @media (max-width: 768px) {
+        display: none;
+    } 
 `;
